adcbe2f73fade9de9b7c797a908c584af0ec17f1
Daniel Tran <trandaniel82@gmail.com>
2025-10-12 10:49:42 -0700

    Updated source.cpp
    added setters, getters, and constructors for student class

--- source.cpp
+++ source.cpp
@@ -1,4 +1,9 @@
 #include "Header.h";
+
+
+
+//***spot section*************
+
 spot::spot() {
 	timelimithours = 0;
 	timelimitminutes = 0;
@@ -60,4 +65,53 @@ float spot::getdue() {
 
 void spot::setpay(bool in) {
 	pay = in;
+}
+
+//*****student section********
+
+student::student() {
+	id = 0;
+	last = "default";
+	first = "default";
+	password = "password";
+}
+
+student::student(int a, string b, string c, string d) {
+	id = a;
+	last = b;
+	first = c;
+	password = d;
+}
+
+
+
+void student::setid(int in) {
+	id = in;
+}
+
+int student::getid() {
+	return id;
+}
+
+
+void student::setlast(string in) {
+	last = in;
+}
+
+
+void student::setfirst(string in) {
+	first = in;
+}
+
+string student::getfirst() {
+	return first;
+}
+
+
+void student::setpassword(string in) {
+	password = in;
+}
+
+string student::getpassword() {
+	return password;
 }
